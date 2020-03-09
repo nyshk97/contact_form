a96c5e65c4c23d801718e648bb6af195aff8cd82
tsubasa namatame <nyshk97@gmail.com>
2020-03-09 12:40:46 +0900

    placeholderを追加

--- index.php
+++ index.php
@@ -25,23 +25,23 @@
   <form action="confirm.php" method="post">
     <div class="form-group">
       <label for="company">会社名</label>
-      <input type="text" name="company" id="company" class="form-control" autofocus required>
+      <input type="text" name="company" id="company" class="form-control" autofocus required placeholder="サンプル株式会社">
     </div>
     <div class="form-group">
       <label for="name">お名前</label>
-      <input type="text" name="name" id="name" class="form-control" required>
+      <input type="text" name="name" id="name" class="form-control" required placeholder="山田太郎">
     </div>
     <div class="form-group">
       <label for="kana">フリガナ</label>
-      <input type="text" name="kana" id="kana" class="form-control" required>
+      <input type="text" name="kana" id="kana" class="form-control" required placeholder="ヤマダタロウ">
     </div>
     <div class="form-group">
       <label for="email">メールアドレス</label>
-      <input type="text" name="email" id="email" class="form-control" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$">
+      <input type="text" name="email" id="email" class="form-control" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" placeholder="test@example.com">
     </div>
     <div class="form-group">
       <label for="phone">電話番号</label>
-      <input type="text" name="phone" id="phone" class="form-control" required pattern="^0\d{9,10}$">
+      <input type="text" name="phone" id="phone" class="form-control" required pattern="^0\d{9,10}$" placeholder="09012345678">
     </div>
     <div class="form-group">
       <label for="content">お問い合わせ内容</label>
